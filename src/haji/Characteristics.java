b9bad5a91b3c69636d76c1a5595fd52c4cae299e
User <User@DESKTOP-TSHD2HJ>
2022-04-26 22:09:52 +0300

    haji

---
+++ src/haji/Characteristics.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package haji;
+
+/**
+ *
+ * @author User
+ */
+public class Characteristics {
+    int age;
+    String colour;
+    String name;
+    double height;
+    
+    
+}
---
+++ src/haji/Haji.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package haji;
+
+/**
+ *
+ * @author User
+ */
+public class Haji {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Characteristics haji=new  Characteristics();
+        haji.age=22;
+        haji.name="haji selemani";
+        haji.colour="white brown";
+        haji.height=6.4;
+        System.out.println("Name is:"+haji.name);
+        System.out.println("his age is"+haji.age);
+        System.out.println("his colour is"+haji.colour);
+        System.out.println("his height is"+haji.height);
+        
+        // TODO code application logic here
+    }
+    
+}
